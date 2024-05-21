706585b8365df06f16e98e491015a6358e5083fc
Joshua-ID <j_joseph69@yahoo.com>
2024-05-21 12:02:51 +0100

    feat: build agency landing page

---
+++ src/components/BusinessProps.vue
@@ -0,0 +1,102 @@
+<template>
+  <div class="businessProps">
+    <div class="img-wrapper" :style="{ background: `${bgColor}` }">
+      <img :src="image" />
+    </div>
+    <div class="business-props-wrapper">
+      <h2>{{ title }}</h2>
+      <p>{{ content }}</p>
+    </div>
+    <button v-if="detailsDefault" class="business-btn">
+      {{ detailsDefault }} <i class="las la-arrow-right"></i>
+    </button>
+    <button v-if="detailsGreen" class="business-green-btn">
+      {{ detailsGreen }} <i class="las la-arrow-right"></i>
+    </button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'BusinessProps',
+
+  props: {
+    title: {
+      type: String
+    },
+    image: {
+      type: String
+    },
+    content: {
+      type: String
+    },
+    detailsDefault: {
+      type: String
+    },
+    detailsGreen: {
+      type: String
+    },
+    bgColor: {
+      type: String,
+      default: ''
+    }
+  }
+}
+</script>
+
+<style scoped>
+.businessProps {
+  width: 230px;
+  font-family: poppins;
+  font-size: 13.7px;
+}
+
+.business-props-wrapper {
+  margin-top: 2.5em;
+  margin-bottom: 2.5em;
+}
+.business-props-wrapper p {
+  margin-top: 2.5em;
+  color: #8e8e8e;
+}
+.img-wrapper {
+  border-radius: 10px;
+  height: 100px;
+  width: 100px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+
+.business-btn {
+  padding: 7px 13px;
+  border: none;
+  border-radius: 16px;
+  background: #fcfcfc;
+  outline: none;
+  cursor: pointer;
+  margin-left: -12px;
+}
+.business-green-btn {
+  /* margin-top: 1.5em; */
+  padding: 7px 13px;
+  border: none;
+  border-radius: 16px;
+  background: green;
+  color: #fff;
+  outline: none;
+  cursor: pointer;
+  margin-left: -12px;
+}
+
+@media (max-width: 800px) {
+  .businessProps {
+    width: 100%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-direction: column;
+    text-align: center;
+  }
+}
+</style>
---
+++ src/components/TestimonialSection.vue
@@ -0,0 +1,108 @@
+<template>
+  <div class="testimonial-section">
+    <ProductInfo title="Testimonial" subTitle="What customers says about us">
+      <template #right-section>
+        <div class="select-button">
+          <i class="las la-chevron-left left-icon"></i>
+          <i class="las la-chevron-right right-icon"></i>
+        </div>
+      </template>
+    </ProductInfo>
+    <div class="testimonial-cards">
+      <TestimonialCards
+        content="Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever"
+        :profileImage="'/public/image/Ellipse 6-1.png'"
+        name="John Peter"
+        description="Vue Developer"
+      />
+      <TestimonialCards
+        content="Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever"
+        :profileImage="'/public/image/Ellipse 6-2.png'"
+        name="John Peter"
+        description="Ceo, card center"
+      />
+      <TestimonialCards
+        content="Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever"
+        :profileImage="'/public/image/Ellipse 6-1.png'"
+        name="John Peter"
+        description="React Developer"
+      />
+      <TestimonialCards
+        content="Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever"
+        :profileImage="'/public/image/Ellipse 6-1.png'"
+        name="John Peter"
+        description="React Developer"
+      />
+    </div>
+  </div>
+</template>
+
+<script>
+import ProductInfo from './ProductInfo.vue'
+import TestimonialCards from './TestimonialCards.vue'
+
+export default {
+  name: 'TestimonialSetion',
+  components: { TestimonialCards, ProductInfo }
+}
+</script>
+
+<style>
+.select-button {
+  display: flex;
+  gap: 16px;
+}
+
+.left-icon,
+.right-icon {
+  padding: 20px;
+  border-radius: 50px;
+}
+
+.la-chevron-left {
+  color: #04b200;
+  background: white;
+}
+.la-chevron-right {
+  color: #fff;
+  background: #04b200;
+}
+
+.testimonial-cards {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  gap: 30px;
+  height: auto;
+  overflow-x: scroll;
+}
+.testimonial-cards::-webkit-scrollbar-thumb {
+  height: 8px;
+  background: #04b200;
+  border-radius: 15px;
+}
+
+.testimonial-section {
+  background: #f1f2fe;
+  padding: 3em 6em;
+}
+
+@media only screen and (max-width: 850px) {
+  .testimonial-cards {
+    display: flex;
+    flex-direction: column;
+  }
+}
+
+@media only screen and (max-width: 740px) {
+  .cardBox {
+    display: flex;
+    flex-direction: column;
+  }
+}
+@media (max-width: 700px) {
+  .testimonial-section {
+    padding: 1.7em;
+  }
+}
+</style>
